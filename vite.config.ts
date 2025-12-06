683aa18b78017563553d71f29eaca962763870ea
marwa154 <marwa154>
2025-12-06 01:18:56 +0100

    Update Vite configuration to set base path and include React plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
+const repoName = "Portfolio";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: `/${repoName}/`,
+  plugins: [react()],
   server: {
     host: "::",
     port: 8080,
   },
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
